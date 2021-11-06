87feef86481c9d23f3e16e38fbf10c7062056a77
youqin <xiaoouvivian.it@gmail.com>
2021-11-07 01:21:39 +0200

    part1 1.6
    *create react project for unicafe
    *implement part1 1.6 (unicafe step1)

---
+++ exercises/part1/unicafe/src/App.js
@@ -0,0 +1,40 @@
+import React, {useState} from 'react'
+
+const Button = ({onclick, text}) => (
+    <button onClick={onclick}>{text}</button>
+)
+
+const App = () => {
+    const [good, setGood] = useState(0)
+    const [neutral, setNeutral] = useState(0)
+    const [bad, setBad] = useState(0)
+
+    const handleGoodClick = () => {
+        setGood(good + 1)
+    }
+
+    const handleNeutralClick = () => {
+        setNeutral(neutral + 1)
+    }
+
+    const handleBadClick = () => {
+        setBad(bad + 1)
+    }
+
+    return (
+        <div>
+            <h1>give feedback</h1>
+            <Button onclick={handleGoodClick} text="good"/>
+            <Button onclick={handleNeutralClick} text="neutral"/>
+            <Button onclick={handleBadClick} text="bad"/>
+
+            <h2>statistics</h2>
+            <p>good {good}</p>
+            <p>neutral {neutral}</p>
+            <p>bad {bad}</p>
+
+        </div>
+    );
+}
+
+export default App;
