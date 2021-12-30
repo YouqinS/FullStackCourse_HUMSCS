b17f5b466794ab65e376a6b76ede4c024ca4ef86
youqin <xiaoouvivian.it@gmail.com>
2021-12-30 05:24:35 +0200

    5.7

--- exercises/part5/bloglist-frontend/src/components/Blog.js
+++ exercises/part5/bloglist-frontend/src/components/Blog.js
@@ -1,8 +1,38 @@
-import React from 'react'
-const Blog = ({blog}) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>  
-)
+import React, {useState} from 'react'
+
+const Blog = ({blog}) => {
+  const [visible, setVisible] = useState(false)
+  const label = visible ? "hide" : "view"
+
+  const showHide = () => {
+    setVisible(!visible)
+  }
+  const blogStyle = {
+    paddingTop: 10,
+    paddingLeft: 2,
+    border: 'solid',
+    borderWidth: 1,
+    marginBottom: 5
+  }
+    return (
+        <div>
+          {
+            visible ?
+                <div style={blogStyle}>
+                  <p>{blog.title}
+                  <button onClick={showHide}> {label} </button></p>
+                  <p>{blog.url}</p>
+                  <p>{blog.likes} <button>like</button></p>
+                  <p>{blog.author}</p>
+                </div>
+                :
+                <div>
+                  {blog.title} {blog.author}
+                  <button onClick={showHide}> {label} </button>
+                </div>
+          }
+        </div>
+    )
+}
 
 export default Blog
